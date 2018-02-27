32a70ee4c50b6ae0eaec5cbeefc16cffd1da2cbf
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-02-27 15:57:25 -0600

    console exercises added and hello world modified

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+//
+//        System.out.print("Enter an integer: ");
+//        int userInt;
+//        userInt = scan.nextInt();
+//        System.out.println("You entered ---> \"" + userInt + "\" <---");
+
+
+
+//        System.out.print("Enter Word: ");
+//        String wordOne = scan.next();
+//        System.out.print("Enter Word: ");
+//        String wordTwo = scan.next();
+//        System.out.print("Enter Word: ");
+//        String wordThree = scan.next();
+//
+//        System.out.println("You entered ---> \"" + wordOne + " " + wordTwo + " " + wordThree + "\" <---");
+
+
+//        System.out.print("Enter a Sentence: ");
+//        String userSentence = scan.nextLine();
+//        System.out.println("You entered ---> \"" + userSentence + "\" <---");
+
+        double length;
+        double width;
+        double height;
+        double perimeter;
+        double area;
+        double volume;
+
+
+        System.out.print("Enter a the classroom's Length, Width and Height: ");
+        length = scan.nextDouble();
+        width = scan.nextDouble();
+        height = scan.nextDouble();
+
+//        length = (int) length;
+//        width = (int) width;
+//        height = (int) height;
+
+        perimeter  = (length * 2) + (width * 2);
+        area = (length * width);
+        volume = area * height;
+
+        System.out.printf("Perimeter: " + "%.2f"  + "m" + " Area: " + "%.2f"  + "m^2" + " Volume: " + "%.2f" +"m^3" + " Length: " + "%.2f" + "m", perimeter, area, volume, length);
+
+//
+//        System.out.print("Enter Num: ");
+//        int option = scan.nextInt();
+//        System.out.print("Holding on to remaining number");
+//        scan.nextLine();  // Consume newline left-over
+//        System.out.print("Enter String: ");
+//        String str1 = scan.nextLine();
+//
+//        System.out.println(option);
+//        System.out.println(str1);
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,61 @@
 public class HelloWorld {
     public static void main (String[] args){
 
-        String myName = "Ben";
-        System.out.println("Hello, " + myName + "!");
+//        String myName = "Ben";
+//        String qtName = "Steven";
+//        System.out.println("Hello, " + qtName + "! " + myName + " loves " + qtName);
+
+//        int myFavoriteNumber = 69;
+//        System.out.println("My Favorite Number: " + myFavoriteNumber); //My Favorite number: 69
+//
+//        String myString = "This is a cool string";
+//
+//        float myNumber = 3.14f;
+//
+//
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //ReWrite Code to shorthand
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 4 + 5;
+
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+//
+//        long n = 461012;
+//        System.out.printf("%d%n", n);      //  -->  "461012"
+//        System.out.printf("%08d%n", n);    //  -->  "00461012"
+//        System.out.printf("%+8d%n", n);    //  -->  " +461012"
+//        System.out.printf("%,8d%n", n);    // -->  " 461,012"
+//        System.out.printf("%+,8d%n%n", n); //  -->  "+461,012"
+//
+//        double pi = Math.PI;
+//
+//        System.out.printf("%f%n", pi);       // -->  "3.141593"
+//        System.out.printf("%.3f%n", pi);     // -->  "3.142"
+//        System.out.printf("%10.3f%n", pi);   // -->  "     3.142"
+//        System.out.printf("%-10.3f%n", pi);  // -->  "3.142"
+
+
     }
 }
