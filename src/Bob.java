7c0bff0e3d32a1cbd68d42a475b3f7cf6afc0c48
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-02-28 14:33:31 -0600

    added StringExercise and Bob.java - a dumb java bot

---
+++ src/Bob.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        boolean askQuestion = true;
+        String bobResponse;
+
+        do {
+            bobResponse = "";
+            System.out.print("Hey Bob: ");
+            String userPrompt = scan.nextLine();
+            if (userPrompt.endsWith("?")){
+                bobResponse += " Sure";
+                System.out.println(bobResponse);
+            } else if (userPrompt.endsWith("!")){
+                bobResponse += " Woah, chill out!";
+                System.out.println(bobResponse);
+            } else if (userPrompt.equals("")){
+                bobResponse += " Fine. Be that way!";
+                System.out.println(bobResponse);
+            } else if (userPrompt.equalsIgnoreCase("bye")) {
+                bobResponse += " Later Gator";
+                System.out.println(bobResponse);
+                askQuestion = false;
+//                break;
+            } else {
+                bobResponse += " Whatever dude...";
+                System.out.println(bobResponse);
+            }
+        } while (askQuestion);
+
+
+    }
+}
